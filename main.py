c1d43709ed3813fb0b0a0b73e328751e7afd6cd5
hwang h <hehehwang@gmail.com>
2021-10-06 23:49:35 +0900

    testing concepts

---
+++ main.py
@@ -0,0 +1,61 @@
+from datetime import datetime, time, timedelta
+from itertools import chain
+
+from git import Actor, Repo
+from pyfiglet import Figlet
+from configparser import ConfigParser
+
+CONFIG = ConfigParser()
+CONFIG.read('config.ini')
+
+garden = Repo('./garden')
+gardenIdx, gardenGit = garden.index, garden.index
+gardener = Actor(CONFIG['Account']['name'], CONFIG['Account']['email'])
+
+
+def commitOnDatetime(dateTime):
+    print(f"{dateTime.year}-{dateTime.month}-{dateTime.day}: Marked")
+    gardenIdx.commit("message", author=gardener, committer=gardener,
+                     author_date=dateTime.isoformat(), commit_date=dateTime.isoformat())
+
+
+def fillTwoYears(commits=1):
+    thisYear = datetime.today().year
+    DtCursor = datetime(thisYear-1, 1, 1, 12, 0, 0)
+    while (DtCursor.year, DtCursor.month, DtCursor.day) != (thisYear, 12, 31):
+        for i in range(commits):
+            commitOnDatetime(DtCursor)
+        DtCursor += timedelta(days=1)
+
+
+def fillWithLetters():
+    def transposeAndFlatten(arr):
+        return chain(*[list(x) for x in zip(*arr)])
+
+    converter = Figlet(font='3x5')
+    while 1:
+        letter = input('Write a letters (1~13 length): ')
+        if 0 < len(letter) <= 13:
+            convertedLetter = converter.renderText(letter)
+            print("Confirm your sentence: ")
+            print(convertedLetter)
+            yesOrNo = input("[Y]/N ")
+            if yesOrNo in {'Y', 'y', ''}:
+                break
+
+    convertedLetterLst = convertedLetter.split('\n')
+    convertedLetterLst[-1] = ' '*len(convertedLetterLst[0])
+    DtCursor = datetime.today()
+    DtCursor = DtCursor.replace(DtCursor.year-1, hour=12,
+                                minute=0, second=0, microsecond=0)
+    DtCursor -= timedelta(days=DtCursor.weekday())
+
+    markers = transposeAndFlatten(convertedLetterLst)
+    for m in markers:
+        if m == '#':
+            commitOnDatetime(DtCursor)
+        DtCursor += timedelta(days=1)
+
+
+if __name__ == '__main__':
+    fillWithLetters()
